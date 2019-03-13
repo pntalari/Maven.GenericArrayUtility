25b24b5ce230bdb67d69f012a1bcfffa6f916f1e
Nirmala <nirmala.talari@gmail.com>
2019-03-12 23:44:47 -0400

    WIP

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,10 +1,10 @@
 package com.zipcodewilmington.arrayutility;
 
+import sun.tools.jconsole.inspector.XObject;
+
+import java.io.ObjectStreamConstants;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility<E> {
+public class ArrayUtility<E extends Object> {
     E[] arr;
 
     public ArrayUtility(E[] inputArray) {
@@ -25,19 +25,28 @@ public class ArrayUtility<E> {
         System.arraycopy(arrayToMerge,0,mergedArr,0,arrayToMerge.length);
         System.arraycopy(this.arr,0,mergedArr,arrayToMerge.length,this.arr.length);
 
-        E result = Arrays.stream(mergedArr)
-                .filter(n -> n==valueToEvaluate).count();
+        E result = (E) (((Object) Arrays.stream(mergedArr)
+                .filter(n -> n == valueToEvaluate).count()));
 
         return result;
 
     }
 
     public E getMostCommonFromMerge(E[] arrayToMerge) {
-        return null; //Arrays.stream(arr);
+        final E[] mergedArr = (E[]) java.lang.reflect.Array.newInstance(arrayToMerge.getClass().
+                getComponentType(),arrayToMerge.length+this.arr.length);
+        System.arraycopy(arrayToMerge,0,mergedArr,0,arrayToMerge.length);
+        System.arraycopy(this.arr,0,mergedArr,arrayToMerge.length,this.arr.length);
+
+       // Map<String,E> map = Arrays.stream(mergedArr)
+          //      .collect(Collectors.groupingBy(Objects::toString, Collectors.counting()));
+
+        return null;
     }
 
     public E getNumberOfOccurrences(E valueToEvaluate) {
-        return null; // Arrays.stream(arr).filter(val->val==valueToEvaluate).count();
+
+        return (E)(Object) (Arrays.stream(arr).filter(val->val==valueToEvaluate).count());
     }
 
     public E[] removeValue(E valueToRemove) {
